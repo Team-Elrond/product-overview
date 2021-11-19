2d8f031f6013e3bdc5f7f9bbb59111a14761b378
Anthony Psyk <ajpsyk@gmail.com>
2021-11-19 14:59:28 -0800

    Updated list products to use count = 5 and page = 1 by default when no params are given

--- routes/listproducts.js
+++ routes/listproducts.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const client = require('../database/db');
 
-var router = express.Router();
+var listProducts = express.Router();
 
-router.get('/products', async (req, res) => {
+listProducts.get('/products', async (req, res) => {
   const listProductsQuery = 'SELECT name, slogan, description, category, default_price FROM products LIMIT $1 OFFSET $2;';
-  const params = [req.query.count, (req.query.page - 1) * req.query.count];
-  const results = await client.query(listProductsQuery, params);
-  res.send(results.rows);
+  const count = req.query.count || 5;
+  const page = req.query.page || 1;
+  const params = [count, (page - 1) * count];
+  const products = await client.query(listProductsQuery, params);
+  res.send(products.rows);
 });
 
-module.exports = router;
+module.exports = listProducts;
