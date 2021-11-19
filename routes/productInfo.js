fa882aa53d1fdb9d1f80c96fe321397275886345
Anthony Psyk <ajpsyk@gmail.com>
2021-11-19 15:03:46 -0800

    Fixed linting issues

--- routes/productInfo.js
+++ routes/productInfo.js
@@ -3,15 +3,14 @@ const client = require('../database/db');
 
 const productInfo = express.Router();
 
-
 productInfo.get('/products/:product_Id', async (req, res) => {
   const productInfoQuery = 'SELECT id, name, slogan, description, category, default_price FROM products WHERE id = $1';
   const featuresQuery = 'SELECT feature, value FROM features WHERE product_id = $1';
   const product = await client.query(productInfoQuery, [req.params.product_Id]);
   const features = await client.query(featuresQuery, [req.params.product_Id]);
 
-  let productWithFeatures = product.rows[0];
-  productWithFeatures['features'] = features.rows;
+  const productWithFeatures = product.rows[0];
+  productWithFeatures.features = features.rows;
   res.send(productWithFeatures);
 });
 
